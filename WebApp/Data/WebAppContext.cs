60b37a422320378258dd2638d4042d1b5611468c
Joe <joantpat@gmail.com>
2025-08-11 00:55:39 +0100

    Added Configuration UI

--- WebApp/Data/WebAppContext.cs
+++ WebApp/Data/WebAppContext.cs
@@ -14,6 +14,44 @@ namespace WebApp.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<JobProvider>().HasData(
+                new JobProvider() 
+                {
+                    Id = 1,
+                    Name = "BAE Systems", 
+                    Url = "https://jobsearch.baesystems.com/search-and-apply",
+                    Class_JobTitle = "div.job-card > h3",
+                    Class_JobLink = "a.job-card__link",
+                    Class_JobDescription = "div.job-card__text > p",
+                    Class_JobLocation = "div.job-card__location",
+                },
+                new JobProvider()
+                {
+                    Id = 2,
+                    Name = "Revolut",
+                    Url = "https://www.revolut.com/careers/",
+                    Class_JobTitle = "div.job-card > h3",
+                    Class_JobLink = "a.job-card__link",
+                    Class_JobDescription = "div.job-card__text > p",
+                    Class_JobLocation = "div.job-card__location"
+                },
+                new JobProvider()
+                {
+                    Id = 3,
+                    Name = "Nestle",
+                    Url = "https://www.nestle.com/jobs/search-jobs",
+                    Class_JobTitle = "a.jobs-title > b",
+                    Class_JobLink = "div.jobs-title > b > a",
+                    Class_JobLocation = "div.jobs-location > small",
+                    Class_NextPage = "div.pager__item--next"
+                }
+            );
+        }
+
         public DbSet<WebApp.Models.Job> Job { get; set; } = default!;
         public DbSet<WebApp.Models.JobProvider> JobProvider { get; set; } = default!;
         public DbSet<WebApp.Models.JobDescription> JobDescription { get; set; } = default!;
---
+++ WebApp/Migrations/20250810180759_SeedData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "JobProvider",
+                columns: new[] { "Id", "Class_JobDescription", "Class_JobLink", "Class_JobLocation", "Class_JobTitle", "Class_NextPage", "KeyWords", "LastScraped", "Name", "Url" },
+                values: new object[,]
+                {
+                    { 1, "div.job-card__text > p", "a.job-card__link", "div.job-card__location", "div.job-card > h3", null, null, new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), "BAE Systems", "https://jobsearch.baesystems.com/search-and-apply" },
+                    { 2, "div.job-card__text > p", "a.job-card__link", "div.job-card__location", "div.job-card > h3", null, null, new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), "Revolut", "https://www.revolut.com/careers/" },
+                    { 3, null, "div.jobs-title > b > a", "div.jobs-location > small", "a.jobs-title > b", "div.pager__item--next", null, new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), "Nestle", "https://www.nestle.com/jobs/search-jobs" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "JobProvider",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "JobProvider",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "JobProvider",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
